11656bb64ecd4f2fd013ac1f38f8cf63f5d952e5
Ginne Abhinaya Sri <60144898+ginnereddy@users.noreply.github.com>
2020-02-20 09:52:17 -0500

    Add files via upload

---
+++ CleanCode/src/main/java/Task4/CompoundInterest.java
@@ -0,0 +1,9 @@
+package Task4;
+
+public class CompoundInterest
+{
+	double compoundInterestCalculator(float principle,float time,float rateOfInterest)
+	{
+		return (principle*Math.pow(1+(rateOfInterest/100),time));
+	}
+}
---
+++ CleanCode/src/main/java/Task4/SimpleInterest.java
@@ -0,0 +1,9 @@
+package Task4;
+
+public class SimpleInterest
+{
+	float simpleInterestCalculator(float principle,float time,float rateOfInterest)
+	{
+		return ((principle*time*rateOfInterest)/100);
+	}
+}
